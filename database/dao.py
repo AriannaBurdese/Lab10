d49a9a9861292cda2e9920abaebcd80b0583c16f
AriannaBurdese <s340730@studenti.polito.it>
2025-11-26 14:27:17 +0100

    prima modifica

--- database/dao.py
+++ database/dao.py
@@ -1,7 +1,44 @@
 from database.DB_connect import DBConnect
+from model.hub import Hub
+
 
 class DAO:
     """
     Implementare tutte le funzioni necessarie a interrogare il database.
     """
-    # TODO
+    @staticmethod
+    def get_hub() -> list[Hub] | None:
+        cnx = DBConnect.get_connection()
+        result = []
+        if cnx is None:
+            print("❌ Errore di connessione al database.")
+            return None
+        cursor = cnx.cursor()
+        query = ("""SELECT "
+                    FROM  
+                    
+                    GROUP BY """)
+        try:
+            cursor.execute(query)
+            for row in cursor:
+                hub = Hub(
+                    id = row["id"],
+                    codice = row["codice"],
+                    nome = row["nome"],
+                    citta = row["citta"],
+                    stato = row["stato"],
+                    latitudine = row["latitudine"],
+                    longitudine = row["longitudine"],
+                )
+                result.append(hub)
+        except Exception as e:
+            print(f"Errore durante la query: {e}")
+            result = None
+        finally:
+            cursor.close()
+            cnx.close()
+        return result
+
+
+
+
